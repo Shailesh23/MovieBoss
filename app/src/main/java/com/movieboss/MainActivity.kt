769c2bac0045c6f3912df9c059df66b12e90b1e6
Shailesh23 <shaileshpant23@gmail.com>
2019-10-26 19:06:55 +0530

    popular movie listing on home page

--- app/src/main/java/com/movieboss/MainActivity.kt
+++ app/src/main/java/com/movieboss/MainActivity.kt
@@ -7,6 +7,9 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.movieboss.adapters.HomeScreenAdapter
 import com.movieboss.network.MoviesRequest
 import com.movieboss.pojo.Result
 import com.movieboss.viewmodels.HomeViewModel
@@ -16,18 +19,32 @@ import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
+    //TODO add data binding
     lateinit var viewModel: HomeViewModel
+    lateinit var popularMovieList : RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        actionBar?.hide()
+        supportActionBar?.hide()
+
+        popularMovieList = findViewById(R.id.popular_movie_list)
+
+        val linearLayoutManager = LinearLayoutManager(this)
+        linearLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        popularMovieList.layoutManager = linearLayoutManager
+
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         viewModel.popularMovies.observe(this,
-            Observer<List<Result>> { t ->
-                for(i in 0 until t?.size!!) {
-                    println(t[i].title)
-                }
+            Observer<List<Result>> { popularMovieDataList ->
+                val homeScreenAdapter = HomeScreenAdapter(this)
+                homeScreenAdapter.setPopularMovies(popularMovieDataList)
+                popularMovieList.adapter = homeScreenAdapter
+
+                homeScreenAdapter.notifyDataSetChanged()
             })
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
---
+++ app/src/main/java/com/movieboss/adapters/HomeScreenAdapter.kt
@@ -0,0 +1,44 @@
+package com.movieboss.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.movieboss.R
+import com.movieboss.pojo.Result
+
+class HomeScreenAdapter(val context : Context) : RecyclerView.Adapter<HomeScreenViewHolder>() {
+
+    lateinit var listOfPopularMovies : List<Result>
+
+    public fun setPopularMovies(popularMovies : List<Result>) {
+        listOfPopularMovies = popularMovies
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeScreenViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.home_screen_movie_item, null)
+        return HomeScreenViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+       return listOfPopularMovies.size
+    }
+
+    override fun onBindViewHolder(holder: HomeScreenViewHolder, position: Int) {
+        holder.posterTitle.text = listOfPopularMovies[position].title
+        Glide.with(context)
+            .load("https://image.tmdb.org/t/p/w185${listOfPopularMovies[position].posterPath}")
+            .into(holder.posterImage)
+
+    }
+}
+
+class HomeScreenViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    var posterImage : ImageView = view.findViewById(R.id.poster_image)
+    var posterTitle : TextView = view.findViewById(R.id.title)
+}
+
