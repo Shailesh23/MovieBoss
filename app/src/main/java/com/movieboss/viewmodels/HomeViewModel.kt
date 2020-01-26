481c534ac48f15cf48376fc61dced116ae29f9a6
Shailesh <shaileshpant23@gmail.com>
2020-01-26 12:52:41 +0530

    refactored code

--- app/src/main/java/com/movieboss/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/movieboss/viewmodels/HomeViewModel.kt
@@ -8,19 +8,17 @@ import com.movieboss.pojo.movies.MovieResult
 
 class HomeViewModel(application: Application) : AndroidViewModel(application) {
 
-    private var popularMovieReqestPage = 0
-    private var topMovieReqestPage = 0
+    private var popularMovieRequestPage = 1
+    private var topMovieRequestPage = 1
     private val moviesRequest = MoviesRequest() //TODO; DI this
     val popularMovies : MutableLiveData<ArrayList<MovieResult>> = loadPopularMovies()
     val topMovies : MutableLiveData<ArrayList<MovieResult>> = loadTopMovies()
 
     fun loadPopularMovies(): MutableLiveData<ArrayList<MovieResult>> {
-        popularMovieReqestPage++
-        return moviesRequest.getPopularMovies(popularMovieReqestPage)
+        return moviesRequest.getPopularMovies(popularMovieRequestPage++)
     }
 
     fun loadTopMovies() : MutableLiveData<ArrayList<MovieResult>> {
-        topMovieReqestPage++
-        return moviesRequest.getTopRatedMovies(topMovieReqestPage)
+        return moviesRequest.getTopRatedMovies(topMovieRequestPage++)
     }
 }
